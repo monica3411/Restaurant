75384e3b2e37bc944853dd51024f35470dd36c8f
monica3411 <guptamonica901@gmail.com>
2026-02-27 12:24:01 +0530

    Add all files

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-scroll';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import {data} from "../restApi.json";
+
+const Navbar = () => {
+  const [show, setShow] = React.useState(false);
+  return (
+    <nav>
+      <div className="logo">MONICA</div>
+      <div className={show ? "nav-links showmenu" : "nav-links"}>
+        <div className="links">{
+          data[0].navbarLinks.map(element=>{
+            return(
+              <Link to={element.link} key={element.id} spy={true} smooth={true}  duration={500}>
+                {element.title}</Link>
+            );
+          })}</div>
+          <button className="menuBtn">OUR MENU</button>
+      </div>
+      <div className="hamburger" onClick={() => setShow(!show)}>
+        <GiHamburgerMenu/>
+      </div>
+      </nav>
+  )
+}
+
+export default Navbar
